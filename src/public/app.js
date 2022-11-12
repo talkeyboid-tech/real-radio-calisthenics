9526ee5539e885c88a710da41c63e34224a0772f
yoshihiro maeda <yoshihiro.maeda.cc@gmail.com>
2022-11-13 02:57:54 +0900

    #31_add_disp add feature initial display and refresh

--- src/public/app.js
+++ src/public/app.js
@@ -11,16 +11,53 @@ window.addEventListener('load', () => {
   // });
 
   const getAll = async () => {
-    const res = await fetch('http://localhost:3000/videos');
+    const res = await fetch(
+      'http://localhost:3000/videos?limit=10&random=true'
+    );
     return await res.json();
   };
 
   getAll().then((res) => {
+    const videoAreaEl = document.querySelector('#video-area');
+    const videoAreaElChild = document.createElement('div');
+    videoAreaElChild.id = 'video-wrapper';
     res.forEach((video) => {
-      const videoAreaEl = document.querySelector('#video-area');
-      const videoAreaElChild = document.createElement('div');
-      videoAreaElChild.textContent = video.title;
+      const iframeEl = document.createElement('iframe');
+      iframeEl.width = '560';
+      iframeEl.height = '315';
+      iframeEl.position = 'absolute';
+      iframeEl.src = `https://www.youtube.com/embed/${video.id}`;
+      iframeEl.title = video.title;
+      iframeEl.setAttribute('frameBorder', 0);
+      iframeEl.allow =
+        'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+      iframeEl.setAttribute('allowfullscreen', '');
+      videoAreaElChild.appendChild(iframeEl);
       videoAreaEl.appendChild(videoAreaElChild);
+    });
+  });
+
+  const refreshButton = document.getElementById('refresh-btn');
+  refreshButton.addEventListener('click', () => {
+    getAll().then((res) => {
+      const videoAreaEl = document.querySelector('#video-area');
+      videoAreaEl.querySelector('#video-wrapper').remove();
+      const videoAreaElChild = document.createElement('div');
+      videoAreaElChild.id = 'video-wrapper';
+      res.forEach((video) => {
+        const iframeEl = document.createElement('iframe');
+        iframeEl.width = '560';
+        iframeEl.height = '315';
+        iframeEl.position = 'absolute';
+        iframeEl.src = `https://www.youtube.com/embed/${video.id}`;
+        iframeEl.title = video.title;
+        iframeEl.setAttribute('frameBorder', 0);
+        iframeEl.allow =
+          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+        iframeEl.setAttribute('allowfullscreen', '');
+        videoAreaElChild.appendChild(iframeEl);
+        videoAreaEl.appendChild(videoAreaElChild);
+      });
     });
   });
 
